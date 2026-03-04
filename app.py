f4e14faf81ed3c0489ad54cf53fe1125040bbd83
poveok177-alt <poveok177@gmail.com>
2026-03-04 19:55:23 +0300

    в

--- app.py
+++ app.py
@@ -26,7 +26,10 @@ def health():
     return "OK", 200
 app.config['SECRET_KEY'] = 'mojasupertajnayastrokakotoruyaniktonevzlomaet123'
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///calories.db')
+database_url = os.environ.get('DATABASE_URL', 'sqlite:///calories.db')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
