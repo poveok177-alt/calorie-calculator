8534cfa4c69ab6418acf15c576c7152beeb9c533
poveok177-alt <poveok177@gmail.com>
2026-03-07 16:45:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from openfoodfacts_search import search_openfoodfacts
+
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
